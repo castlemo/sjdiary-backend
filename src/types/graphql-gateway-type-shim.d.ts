882865cdbc28e56982c552868de2f6d53b1e59fc
castlemo <eodah4115@gmail.com>
2021-10-17 23:18:35 +0900

    nestjs graphql error Fix

---
+++ src/types/graphql-gateway-type-shim.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-interface */
+declare module '@apollo/gateway' {
+  export interface GatewayConfig {}
+  export interface ServiceEndpointDefinition {}
+}
+
+declare module '@apollo/gateway/dist/datasources/types' {
+  export interface GraphQLDataSource {}
+}
